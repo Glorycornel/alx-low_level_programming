ea16f08b0a5c7bb4433d903c051c90eb70ff0ac3
Glorycornel <chiomaowanku@gmail.com>
2022-06-07 12:55:22 -0700

    corrected

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 /**
-* main - Entry point
-* Description: prints '_putchar \n'
-* Return: Always 0 (success)
+* main - expected to print _putchar
+*
+*Return: 0 successfully
 */
 int main(void)
 {
-char str[8] = "_putchar";
+char text[8] = "_putchar";
 int i;
 for (i = 0; i < 8; i++)
 {
-_putchar(str[i]);
+_putchar(text[i]);
 }
 _putchar('\n');
 return (0);
 }
-
